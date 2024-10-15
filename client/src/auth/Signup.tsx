7609a69812893e74602f0ea0edb4dcb4685314f0
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2024-10-15 15:30:41 +0530

    routing nd signup basic form

--- client/src/auth/Signup.tsx
+++ client/src/auth/Signup.tsx
@@ -1,10 +1,136 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+//these loader icon nd mail icon nd taala icon of password are from lucide-react package which is a package of icons shadecn
+import { Contact, Loader2, LockKeyhole, Mail, User } from "lucide-react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-type Props = {}
 
-export default function Signup({}: Props) {
+type SignupInputState = {
+    fullname: string;
+    contact: number;
+    email: string;
+    password: string;
+    };
+
+const Signup = () => {
+  const [input, setInput] = useState<SignupInputState>({
+    email: "",
+    password: "",
+    fullname: "",
+    contact: 0,
+
+  });
+
+
+  const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    //destructuring the name and value from the target of the event object and 
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
+
+  const loginSubmitHandler = async (e: FormEvent) => {
+    e.preventDefault();
+    console.log(input);
+  };
+
+  const loading = true;
+
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen">
+      <form
+        onSubmit={loginSubmitHandler}
+        className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4"
+      >
+        <div className="mb-4">
+          <h1 className="font-bold text-2xl">IndiaEats</h1>
+        </div>
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="text"
+              placeholder="Full Name"
+              name="fullname"
+              value={input.fullname}
+              onChange={changeEventHandler}
+              className="pl-10 focus-visible:ring-1"
+            />
+            <User className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="number"
+              placeholder="Contact"
+              name="contact"
+              value={input.contact}
+              onChange={changeEventHandler}
+              className="pl-10 focus-visible:ring-1"
+            />
+            <Contact className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={input.email}
+              onChange={changeEventHandler}
+              className="pl-10 focus-visible:ring-1"
+            />
+            <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={input.password}
+              onChange={changeEventHandler}
+              className="pl-10 focus-visible:ring-1"
+            />
+            <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+        <div className="mb-10">
+             {/* if loading is true then show the loader icon else show the login button . button is initially disabled if loading is true else it is enabled  */}
+          {loading ? (
+            <Button disabled className="w-full bg-orange hover:bg-hoverOrange">
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
+            </Button>
+          ) : (
+            <Button
+              type="submit"
+              className="w-full bg-orange hover:bg-hoverOrange"
+            >
+              Signup
+            </Button>
+          )}
+          <div className="mt-4">
+            <Link
+              to="/forgot-password"
+              className="hover:text-blue-500 hover:underline"
+            >
+              Forgot Password
+            </Link>
+          </div>
+        </div>
+        <Separator />
+        <p className="mt-2">
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-500">
+            Signup
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
 
+export default Signup;
