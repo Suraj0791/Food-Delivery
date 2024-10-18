ac33e7fc9bcd4bb5994d5aa1262d338670d393b5
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2024-10-18 19:01:44 +0530

    mongoDB connected with basic ts setup (check all json nd tsconfig file)

---
+++ server/src/config/index.ts
@@ -0,0 +1,4 @@
+import { ServerConfig } from './server-config.ts';
+import Logger from './logger-config.ts';
+
+export { ServerConfig, Logger };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,16 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+import express from 'express';
+import connectDB from './db/connectDB.ts';
+import { ServerConfig } from './config/index.ts';
 
+const app = express();
+const port = ServerConfig.PORT || 3000;
+
+// Connect to MongoDB
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
