d433c09991f7ec3a447ca96d1d79abdb44d72e8c
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2024-10-19 12:25:08 +0530

    done

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,18 +2,26 @@ import express from 'express';
 import connectDB from './db/connectDB.ts';
 import { ServerConfig } from './config/index.ts';
 import  apiRoutes from './routes/index.ts';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
-// Connect to MongoDB
-connectDB();
 
+
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(cors());
+
 
 app.use('/api', apiRoutes);
 
 
 app.listen(ServerConfig.PORT, () => {
+  // Connect to MongoDB
+connectDB();
   console.log(`Server is running on port ${ServerConfig.PORT}`);
 });
